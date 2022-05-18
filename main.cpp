d5cecc8b8c694b597af765350d1b090c7ffc1542
Miguel Alonso <miguelangel.alonso@nxp.com>
2022-05-18 09:18:25 -0700

    Remove unused code in main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@ int main(int argc, char *argv[])
 {
     std::cout << "Start btplayer..." << std::endl;
     qmlRegisterType<Player>("com.nxp.btplayer", 1, 0, "PlayerModel");
-    //qmlRegisterType<Player>("com.nxp.btplayer", 1, 0, "mediaModel");
 
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
@@ -25,32 +24,5 @@ int main(int argc, char *argv[])
     if(!mediaWrapper.initialize())
         return -1;
 
-
-
-    /*QQmlApplicationEngine engine;
-
-
-
-    Player mPlayer;
-    QList<QObject *> mediaList;
-    mediaList.append(new Media("SongSample", "ArtistSample", "AlbumSample", "ArtSample", 4.00));
-    //mediaList.append(new Media("SongSample1", "ArtistSample1", "AlbumSample1", "ArtSample1", 4.10));
-    //mediaList.append(new Media("SongSample2", "ArtistSample2", "AlbumSample2", "ArtSample2", 4.20));
-    //mediaList.append(new Media("SongSample3", "ArtistSample3", "AlbumSample3", "ArtSample3", 4.30));
-
-    engine.rootContext()->setContextProperty("PlayerModel", QVariant::fromValue(&mPlayer));
-    engine.rootContext()->setContextProperty("mediaModel", QVariant::fromValue(mediaList));
-
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
-*/
     return app.exec();
 }
-
-    /*if(engine.rootObjects().isEmpty())
-        return -1;*/
