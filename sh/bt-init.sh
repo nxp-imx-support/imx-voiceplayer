790eeb5d8d3ec9500f98908fad47343bc82763f9
Julio Meza <juliopablo.meza@nxp.com>
2022-06-08 16:00:14 -0700

    Update bt-init.sh
    Added set sink and source in pulseaudio.

--- sh/bt-init.sh
+++ sh/bt-init.sh
@@ -20,21 +20,6 @@ Bluetooth () {
                 hciconfig hci0 noauth
                 sdptool add SP
                 sleep 1
-
-                # Automatic connection
-                output="";
-                coproc bluetoothctl
-                for (( a=1; a<4; a++ ))
-                do
-                read output <&${COPROC[0]}
-                echo "$output";
-                done
-                sleep 4
-                for (( b=1; b<11; b++ ))
-                do
-                echo -e 'yes\n' >&${COPROC[1]}
-                sleep .5
-                done
 }
 
 # Check if Bluetooth was previously configured
@@ -49,12 +34,93 @@ else
 evk=$(uname -n);
 echo -e "Evk:${evk}";
 
-        if  [[ $evk == "imx8ulp-lpddr4-evk" || $evk == "imx8ulpevk" ]]
+# Start pulseaudio server
+pulseaudio --start --log-target=syslog
+echo "Pulseaudio server successfully started";
+
+        if  [[ $evk == "imx8mnevk" || $evk == "imx8mmevk" || $evk == "imx8mm-lpddr4-evk" || $evk == "imx8mm-ddr4-evk" || $evk == "imx8mn-lpddr4-evk" || $evk == "imx8mn-ddr4-evk" ]]
         then
-                # Bluetooth function
-                Bluetooth LP2
-        else
-                # Bluetooth function
+                # Get sink pulse audio index
+                sink_index=$(pacmd list-sinks | grep -B1 "name: <alsa_output.platform-sound-wm8524*" | sed '$d' | cut -d " " -f 5);
+                if  [[ $sink_index == "" || $sink_index == "index:" ]]
+                then
+                        sink_index=$(pacmd list-sinks | grep -B1 "name: <alsa_output.platform-sound-wm8524*" | sed '$d' | cut -d " " -f 6);
+                fi
+                echo -e "Sink index:${sink_index}";
+
+                # Set default sink
+                pacmd set-default-sink ${sink_index}
+
+                # Get source pulse audio index
+                source_index=$(pacmd list-sources | grep -B1 "name: <alsa_input.platform-sound-bt*" | sed '$d' | cut -d " " -f 5);
+                if  [[ $source_index == "" || $source_index == "index:" ]]
+                then
+                        source_index=$(pacmd list-sources | grep -B1 "name: <alsa_input.platform-sound-bt*" | sed '$d' | cut -d " " -f 6);
+                fi
+                echo -e "Source index:${source_index}";
+
+                # Set default source
+                pacmd set-default-source ${source_index}
+
+		# Bluetooth function
                 Bluetooth mxc0
+
+        elif  [[ $evk == "imx8mpevk" || $evk == "imx8mp-ddr4-evk" || $evk == "imx8mp-lpddr4-evk" ]]
+        then
+                # Get sink pulse audio index
+                sink_index=$(pacmd list-sinks | grep -B1 "name: <alsa_output.platform-sound-wm8960*" | sed '$d' | cut -d " " -f 5);
+                if  [[ $sink_index == "" || $sink_index == "index:" ]]
+                then
+                        sink_index=$(pacmd list-sinks | grep -B1 "name: <alsa_output.platform-sound-wm8960*" | sed '$d' | cut -d " " -f 6);
+                fi
+                echo -e "Sink index:${sink_index}";
+
+                # Set default sink
+                pacmd set-default-sink ${sink_index}
+
+                # Get source pulse audio index
+                source_index=$(pacmd list-sources | grep -B1 "name: <alsa_input.platform-sound-bt*" | sed '$d' | cut -d " " -f 5);
+                if  [[ $source_index == "" || $source_index == "index:" ]]
+                then
+                        source_index=$(pacmd list-sources | grep -B1 "name: <alsa_input.platform-sound-bt*" | sed '$d' | cut -d " " -f 6);
+                fi
+                echo -e "Source index:${source_index}";
+
+                # Set default source
+                pacmd set-default-source ${source_index}
+
+		# Bluetooth function
+		Bluetooth mxc0
+
+        elif  [[ $evk == "imx8ulp-lpddr4-evk" || $evk == "imx8ulpevk" ]]
+        then
+                # Get sink pulse audio index
+                sink_index=$(pacmd list-sinks | grep -B1 "name: <alsa_output.platform-imx-audio-rpmsg*" | sed '$d' | cut -d " " -f 5);
+                if  [[ $sink_index == "" || $sink_index == "index:" ]]
+                then
+                        sink_index=$(pacmd list-sinks | grep -B1 "name: <alsa_output.platform-imx-audio-rpmsg*" | sed '$d' | cut -d " " -f 6);
+                fi
+                echo -e "Sink index:${sink_index}";
+
+                # Set default sink
+                pacmd set-default-sink ${sink_index}
+
+                # Get source pulse audio index
+                source_index=$(pacmd list-sources | grep -B1 "name: <alsa_input.platform-sound-bt*" | sed '$d' | cut -d " " -f 5);
+                if  [[ $source_index == "" || $source_index == "index:" ]]
+                then
+                        source_index=$(pacmd list-sources | grep -B1 "name: <alsa_input.platform-sound-bt*" | sed '$d' | cut -d " " -f 6);
+                fi
+                echo -e "Source index:${source_index}";
+
+                # Set default source
+                pacmd set-default-source ${source_index}
+
+		# Bluetooth function
+		Bluetooth LP2
+
+	else
+        echo "Unsupported EVK";
+
         fi
 fi
