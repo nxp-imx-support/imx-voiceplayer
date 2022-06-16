e201644c265c65075546fc31c4e7a647cebab168
Julio Meza <juliopablo.meza@nxp.com>
2022-06-16 14:04:37 -0700

    Add support to enable VoiceSeeker

---
+++ sh/Enable_VoiceSeeker.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+# Enable VoiceSeeker Audio Front-End #
+
+# Copy asound.conf
+cp -v /etc/asound.conf /etc/asound.conf_original
+cp -v /opt/Btplayer/bin/asound.conf /etc
+
+# Copy Voiceseekerlight library
+cp -v /opt/Btplayer/bin/libvoiceseekerlight.so.1.0 /usr/lib/nxp-afe
+ln -s libvoiceseekerlight.so.1.0 /usr/lib/nxp-afe/libvoiceseekerlight.so
+
+# Copy Config.ini file
+cp -v /opt/Btplayer/bin/Config.ini /unit_tests/nxp-afe
