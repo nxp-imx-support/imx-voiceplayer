a172866e54351d1d097258b0a4f4524c3e89be83
Miguel Alonso <miguelangel.alonso@nxp.com>
2022-10-25 11:31:54 -0700

    Update patch target sysroot

--- build-demo.sh
+++ build-demo.sh
@@ -42,13 +42,13 @@ cd imx-voiceui/vit/i.MX8M_A53
 git apply 0001-to-generate-patch.patch
 make -j8
 echo "VIT compilation ended"
-cd ../..
+cd ../../..
 
 #### PACK DEMO COMPONENTS ####
 
 cp -a build/Btplayer ${BUILD_OUTPUT_DIR}
 cp -a msgq/build/MsgQ ${BUILD_OUTPUT_DIR}
-cp -a imx-voiceui/vit/build/btp_vit ${BUILD_OUTPUT_DIR}
+cp -a imx-voiceui/vit/i.MX8M_A53/build/btp_vit ${BUILD_OUTPUT_DIR}
 cp -a sh/*.sh ${BUILD_OUTPUT_DIR}
 
 #cd ..
