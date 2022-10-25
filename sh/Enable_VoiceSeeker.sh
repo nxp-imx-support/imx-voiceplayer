48fc575c263a61db2dc40e53bd57cb755b3337fb
Julio Meza <juliopablo.meza@nxp.com>
2022-10-25 09:25:32 -0700

    Remove binaries and backup Config.ini

--- sh/Enable_VoiceSeeker.sh
+++ sh/Enable_VoiceSeeker.sh
@@ -6,13 +6,7 @@
 cp -v /etc/asound.conf /etc/asound.conf_original
 cp -v /opt/Btplayer/bin/asound.conf /etc
 
-# Copy Voiceseekerlight library
-cp -v /opt/Btplayer/bin/libvoiceseekerlight.so /usr/lib/nxp-afe/
-ln -s libvoiceseekerlight.so.1.0 /usr/lib/nxp-afe/libvoiceseekerlight.so
-
 # Copy Config.ini file
+cp -v /unit_tests/nxp-afe/Config.ini /unit_tests/nxp-afe/Config.ini_original
 cp -v /opt/Btplayer/bin/Config.ini /unit_tests/nxp-afe
 
-# Copy NXP binaries
-cp -v /opt/Btplayer/bin/HeyNXP_en-US_1.bin /unit_tests/nxp-afe
-cp -v /opt/Btplayer/bin/HeyNXP_1_params.bin /unit_tests/nxp-afe 
