ab1304afa7fa4ec4f839bbe0b9c06dadb2a21d25
Alberto Alvarez <alberto.alvarez@nxp.com>
2024-08-29 15:11:15 -0700

    Updated SCR and copyrights

--- scripts/Enable_VoiceSeeker.sh
+++ scripts/Enable_VoiceSeeker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2022-2023 NXP                                                                                                                                                                                                                                                                            
+# Copyright 2022-2024 NXP
 # SPDX-License-Identifier: BSD-3-Clause
 
 # Enable VoiceSeeker Audio Front-End
